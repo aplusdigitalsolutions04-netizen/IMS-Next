09d378b2ee9fe8e19aeaeff0915dfb3589d1814c
rahul2083 <udayjangid3820@gmail.com>
2026-07-27 10:20:32 +0530

    Update vendor migration script

---
+++ scripts/migrate_vendor_data.js
@@ -0,0 +1,19 @@
+﻿const mysql = require('mysql2/promise');
+async function migrate() {
+  const c = await mysql.createConnection({host:'localhost', user:'Rahul', password:'Rahul@3820'});
+  const cGuid = '013ccc62-9170-40cb-a109-1c968ccfdfbf';
+  try {
+    await c.query('TRUNCATE TABLE newdb.inventoryvendor');
+    const [cols] = await c.query('DESCRIBE imnew.inventoryvendor');
+    const colNames = cols.map(c => c.Field);
+    const insertCols = [...colNames, 'companyGuid'].join(', ');
+    const selectCols = colNames.join(', ');
+    const query = 'INSERT INTO newdb.inventoryvendor (' + insertCols + ') SELECT ' + selectCols + ', ? FROM imnew.inventoryvendor';
+    await c.query(query, [cGuid]);
+    console.log('Migrated inventoryvendor successfully');
+  } catch (err) {
+    console.error('Error migrating inventoryvendor:', err.message);
+  }
+  c.end();
+}
+migrate();
